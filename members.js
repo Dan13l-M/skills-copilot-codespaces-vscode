b147ee83b1fa8c5683a4ac32e76a829c6cf07b85
Dan13l-M <130105178+Dan13l-M@users.noreply.github.com>
2023-11-10 06:37:56 +0000

    Copilot second commit

---
+++ members.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    return {
+        name: "skills",
+        description: "Get the skills of a member",
+        execute: function (msg, args) {
+            msg.channel.send("Not implemented yet")
+        }
+    }
+}
